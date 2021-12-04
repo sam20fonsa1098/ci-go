b8d1943d80b5357d106418b519ae6b49c0d8efef
Samuel Cristo da Fonseca <samfonsa12345@gmail.com>
2021-12-03 21:10:00 -0400

    fix: :bug: fixing testsum

--- math_test.go
+++ math_test.go
@@ -1,12 +1,11 @@
 package main
 
 import "testing"
-import "./math"
 
 func TestSum(t *testing.T) {
 	total := Sum(5, 5)
 
-	if total != 30 {
-		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 30);
+	if total != 10 {
+		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 10);
 	}
 }
