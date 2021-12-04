b45bc8e227e6b33c43f36d42b536c2f6022a6565
Samuel Cristo da Fonseca <samfonsa12345@gmail.com>
2021-12-04 10:09:45 -0400

    ci: :art: change ci file to trigger only in develop branch

--- math_test.go
+++ math_test.go
@@ -6,6 +6,6 @@ func TestSum(t *testing.T) {
 	total := Sum(5, 5)
 
 	if total != 10 {
-		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 10);
+		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 10)
 	}
 }
